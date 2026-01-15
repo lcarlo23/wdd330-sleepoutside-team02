ea96b5fec5e474fa0babdcb79826eb9ebf2991c9
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-01-15 18:32:32 +0100

    Addded discount indicator in product listing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,6 +1,20 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+
+    let price = `<p class="product-card__price">$${product.FinalPrice}</p>`;
+
+    if (product.FinalPrice < product.SuggestedRetailPrice) {
+        const discount = (product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice * 100;
+        price = `
+        <p class="discount-tag">${Math.round(discount)}% OFF</p>
+        <p class="product-card__discount">
+            <span class="discount-price">$${product.FinalPrice}</span>
+            <span class="original-price">$${product.SuggestedRetailPrice}</span>
+        </p>
+        `
+    }
+
     return `
     <li class="product-card">
         <a href="product_pages/?product=${product.Id}">
@@ -9,7 +23,7 @@ function productCardTemplate(product) {
             />
             <h3 class="card__brand">${product.Brand.Name}</h3>
             <h2 class="card__name">${product.NameWithoutBrand}</h2>
-            <p class="product-card__price">$${product.FinalPrice}</p>
+            ${price}
         </a>
     </li>
     `;
