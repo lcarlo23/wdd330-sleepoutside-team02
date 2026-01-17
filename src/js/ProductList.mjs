146698d22a4447876d9dacf899f5e4ccfc16caee
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2026-01-17 17:04:09 -0600

    Refactor productCardTemplate and ProductList class
    Refactor productCardTemplate for better readability and add alt attribute to image. Update ProductList class constructor formatting.

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,43 +1,33 @@
+//ProductList.msj
 import { renderListWithTemplate } from "./utils.mjs";
 
+/*Create a template function that will simply return a template literal 
+string for each of the templates needed. Add a function for a productCardTemplate(product). 
+You can use the current HTML in the /index.html file as your starting point.*/
 function productCardTemplate(product) {
-
-    let price = `<p class="product-card__price">$${product.FinalPrice}</p>`;
-
-    if (product.FinalPrice < product.SuggestedRetailPrice) {
-        const discount = (product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice * 100;
-        price = `
-        <p class="discount-tag">${Math.round(discount)}% OFF</p>
-        <p class="product-card__discount">
-            <span class="discount-price">$${product.FinalPrice}</span>
-            <span class="original-price">$${product.SuggestedRetailPrice}</span>
-        </p>
-        `
-    }
-
-    return `
-    <li class="product-card">
-        <a href="product_pages/?product=${product.Id}">
-            <img
-                src="${product.Image}"
-            />
-            <h3 class="card__brand">${product.Brand.Name}</h3>
-            <h2 class="card__name">${product.NameWithoutBrand}</h2>
-            ${price}
-        </a>
-    </li>
-    `;
+  return `<li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+      <img src="${product.Image}" alt="${product.Brand.Name}">
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>`;
 }
 
-export default class ProductList {
-    constructor(category, dataSource, listElement) {
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-    }
+/*Add a class called ProductList 
+and export this class as default.*/
 
-    async init() {
-        const list = await this.dataSource.getData();
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    // Pass the category to getData if your ProductData class requires it
+    const list = await this.dataSource.getData();
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 }
