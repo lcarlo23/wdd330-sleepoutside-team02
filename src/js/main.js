d333f698dec719e71493aa208f4b094af99a92e4
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-01-22 12:08:58 +0100

    completed step2 week3 individual activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const category = "tents";
-const productListElement = document.querySelector("ul.product-list");
-
-const dataSource = new ProductData(category);
-const productsList = new ProductList(category, dataSource, productListElement);
-
 loadHeaderFooter();
-productsList.init();
