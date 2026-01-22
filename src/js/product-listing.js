d6edfb0a6e07c226805c86283c70f30073725bc7
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-01-22 11:09:28 +0100

    first commit work in progress

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,6 +1,5 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
 const category = "tents";
 const productListElement = document.querySelector("ul.product-list");
@@ -8,5 +7,4 @@ const productListElement = document.querySelector("ul.product-list");
 const dataSource = new ProductData(category);
 const productsList = new ProductList(category, dataSource, productListElement);
 
-loadHeaderFooter();
 productsList.init();
