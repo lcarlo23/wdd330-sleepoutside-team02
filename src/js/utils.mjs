2b8a37ea4f54e30a77c64bcb2d09da6fa2491fa1
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2026-01-22 21:07:39 -0600

    Implement loadTemplate and loadHeaderFooter functions
    Added functions to load templates and render header/footer.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -42,3 +42,35 @@ export function getParam(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
+export function renderWithTemplate(template, parentElement, data, callback) {
+  // If template is a string (from fetch), set it directly
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback();
+  }
+}
+
+/*Add a function to the utils.mjs named loadTemplate. 
+This asynchronous function fetches the content of the HTML file 
+given a path. The response to the fetch is converted to text 
+and returns the HTML content as a string.*/
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+/*Add a function to the utils.mjs named loadHeaderFooter that can be
+ exported. It needs to be able to do the following:
+ Load the header and footer templates in from the partials using the loadTemplate.
+ Grab the header and footer placeholder elements out of the DOM.
+ Render the header and footer using renderWithTemplate.*/
+export async function loadHeaderFooter() {
+  // Path: Up one level from product_pages, into public/partials
+  const headerTemplate = await loadTemplate("../public/partials/header.html");
+  const headerElement = qs("#main-header");
+  renderWithTemplate(headerTemplate, headerElement);
+
+  const footerTemplate = await loadTemplate("../public/partials/footer.html");
+  const footerElement = qs("#main-footer");
+  renderWithTemplate(footerTemplate, footerElement);
+}
