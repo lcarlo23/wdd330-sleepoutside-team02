680c7bc8875bd154d807170d47733ccb2c0b097a
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2026-01-22 21:04:56 -0600

    Update ProductData.mjs

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,23 +1,20 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
-
+// src/js/ProductData.mjs
 export default class ProductData {
   constructor(category) {
     this.category = category;
     this.path = "/src/public/json/" + category + ".json";
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  // Inside ProductData.mjs constructor or getData method
+  async getData() {
+    // If your index.html is in /src/product_pages/, you need to go up to src:
+    const response = await fetch("../public/json/tents.json");
+    const data = await response.json();
+    return data;
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
   }
 }
+
